45fdc7879b7c8512bceb6fb3ef38f127264c9333
chris <chris.senior@cognitran.com>
2023-11-20 10:33:34 +0000

    Correct plugin filename. Unit test was for demo code!

--- src/test/java/JavaConventionsPluginTest.java
+++ src/test/java/JavaConventionsPluginTest.java
@@ -1,15 +1,19 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class JavaConventionsPluginTest {
   @Test
-  public void greeterPluginAddsGreetingTaskToProject() {
+  public void verifyBasicConventions() {
     Project project = ProjectBuilder.builder().build();
-    project.getPluginManager().apply("org.example.greeting");
-
-    assertTrue(project.getTasks().getByName("hello") instanceof GreetingTask);
+    project.getPluginManager().apply("duckasteroid-java");
+    List<Plugin> plugins = project.getPlugins().stream().toList();
+    assertEquals(13, plugins.size());
+    assertTrue(project.getPluginManager().hasPlugin("java"));
   }
 }
