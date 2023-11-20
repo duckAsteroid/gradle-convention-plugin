65e1640f3d4f26fc16805869399cdf18cb503e3f
chris <chris.senior@cognitran.com>
2023-11-20 10:35:02 +0000

    Use Java 11 toList

--- src/test/java/JavaConventionsPluginTest.java
+++ src/test/java/JavaConventionsPluginTest.java
@@ -6,13 +6,14 @@ import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JavaConventionsPluginTest {
   @Test
   public void verifyBasicConventions() {
     Project project = ProjectBuilder.builder().build();
     project.getPluginManager().apply("duckasteroid-java");
-    List<Plugin> plugins = project.getPlugins().stream().toList();
+    List<Plugin> plugins = project.getPlugins().stream().collect(Collectors.toList());
     assertEquals(13, plugins.size());
     assertTrue(project.getPluginManager().hasPlugin("java"));
   }
